edb64c9a3192ee696faeb70292349538f7b5df9d
Michael Huang <mikhuang@gmail.com>
2016-03-29 21:42:09 -0700

    Create setup.py
    Adding crap setup.py with original author information

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="django-cohorts",
+    version="0.0.1",
+    description="Reusable app for performing cohort analysis on django projects.",
+    long_description=open('readme.txt').read(),
+    author="Alex Kehayias",
+    author_email="alex@shareablee.com",
+    url="https://github.com/alexkehayias/django-cohorts",
+    license='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
